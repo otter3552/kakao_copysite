b77e18d22a617059dabf0c7ca81c26969dd4bc79
otter3552 <mr_shield@naver.com>
2020-04-03 23:36:58 +0900

    kakao 카피 사이트를 위한 뷰 프로젝트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    kakao Copy Site
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
